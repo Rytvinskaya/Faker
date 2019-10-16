89669dd62a451404bdb6f25e3d48a19d8aa7555e
Rytvinskaya <a.rytvinskaya@gmail.com>
2019-10-16 12:51:20 +0300

    Added plugins

---
+++ Plugins/DateTimeGenerator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Faker;
+
+namespace Plugins
+{
+    public class DateTimeGenerator : IGenerator
+    {
+        private static readonly Random Random = new Random();
+        public object Generate(Type type)
+        {
+            if (type != GetGenerationType())
+            {
+                throw new ArgumentException("Returned types does not match");
+            }
+            var buf = new byte[8];
+            Random.NextBytes(buf);
+            var ticks = BitConverter.ToInt64(buf, 0);
+            return new DateTime(Math.Abs(ticks % (DateTime.MaxValue.Ticks - DateTime.MinValue.Ticks)) + DateTime.MinValue.Ticks);
+        }
+
+        public Type GetGenerationType()
+        {
+            return typeof(DateTime);
+        }
+    }
+}
---
+++ Plugins/FloatGenerator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Faker;
+
+namespace Plugins
+{
+    public class FloatGenerator : IGenerator
+    {
+        private static readonly Random Random = new Random();
+        public object Generate(Type type)
+        {
+            if (type != GetGenerationType())
+            {
+                throw new ArgumentException("Returned types does not match");
+            }
+            var mantissa = (Random.NextDouble() * 2.0) - 1.0;
+            var exponent = Math.Pow(2.0, Random.Next(-126, 128));
+            return (float)(mantissa * exponent);
+        }
+
+        public Type GetGenerationType()
+        {
+            return typeof(float);
+        }
+    }
+}
